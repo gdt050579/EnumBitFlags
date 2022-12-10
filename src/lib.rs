7c915aa022484946d7145763911754424e4e8c0c
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-12-10 19:14:14 +0200

    Refactor to use templates for proc_macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ use proc_macro::*;
 
 extern crate proc_macro;
 
+static impl_template: &str = r"
+
+";
+
 enum State {
     ExpectEnum,
     ExpectName,
@@ -21,7 +25,7 @@ struct Parser {
 impl Parser {
     fn new() -> Parser {
         Parser {
-            output: String::new(),
+            output: String::with_capacity(1024),
             name: String::new(),
             state: State::ExpectEnum,
         }
@@ -32,7 +36,7 @@ impl Parser {
             if (txt!="enum") && (txt!="flags") {
                 panic!("Expecting an enum keywork but got: {}",txt);
             }
-            self.output.push_str("struct ");
+            self.output.push_str("struct $$(NAME)$$ { value: u32 }\n impl $$(NAME)$$ {\n");
             self.state = State::ExpectName;
         } else {
             panic!("Expecting an enum keyword but got: {:?}",token);
@@ -41,7 +45,6 @@ impl Parser {
     fn validate_expect_enum_name(&mut self, token: TokenTree) {
         if let TokenTree::Ident(ident) = token {
             self.name = ident.to_string();           
-            self.output.push_str(self.name.as_str());
             self.state = State::ExpectOpenBrace;
         } else {
             panic!("Expecting the name of the enum but got: {:?}",token);
@@ -52,7 +55,6 @@ impl Parser {
             if group.delimiter() != Delimiter::Brace {
                 panic!("Expecting an open brace '{{' after enum name but got {:?}",group.delimiter());
             }
-            self.output.push_str(" {");
             self.state = State::ExpectFlag;
             self.parse(group.stream());
         } else {
@@ -61,9 +63,9 @@ impl Parser {
     } 
     fn validate_expect_flag(&mut self, token: TokenTree) {
         if let TokenTree::Ident(ident) = token {
-            self.name = ident.to_string();  
-            self.output.push_str("\n");         
-            self.output.push_str(self.name.as_str());
+            self.output.push_str("\tpub const ");         
+            self.output.push_str(ident.to_string().as_str());
+            self.output.push_str(": $$(NAME)$$ = $$(NAME)$$ { value: ");         
             self.state = State::ExpectEqual;
         } else {
             panic!("Expecting the name of a flag but got: {:?}",token);
@@ -74,10 +76,9 @@ impl Parser {
             if punctuation.as_char() != '=' {
                 panic!("Expecting equal '=' symbol but got: {}",punctuation.as_char());
             }
-            self.output.push_str(" = ");         
             self.state = State::ExpectValue;
         } else {
-            panic!("Expecting the name of a flag but got: {:?}",token);
+            panic!("Expecting equal '=' symbol but got: {:?}",token);
         }
     } 
     fn validate_expect_value(&mut self, token: TokenTree) {
@@ -92,7 +93,8 @@ impl Parser {
             if value.is_err() {
                 panic!("Expecting an integer value (but got: {})",text);
             }
-            self.output.push_str(&text);                     
+            self.output.push_str(&text);              
+            self.output.push_str(" };\n");       
             self.state = State::ExpectSeparatorOrCloseBrace;
         } else {
             panic!("Expecting the name of a flag but got: {:?}",token);
@@ -102,8 +104,7 @@ impl Parser {
         if let TokenTree::Punct(punctuation) = token {
             if punctuation.as_char() != ',' {
                 panic!("Expecting ',' separator but got: {}",punctuation.as_char());
-            }
-            self.output.push_str(", \n");         
+            }     
             self.state = State::ExpectFlag;
         } else {
             panic!("Expecting ',' separator but got:  {:?}",token);
@@ -122,12 +123,20 @@ impl Parser {
             }
         }    
     }
+    fn add_methods(&mut self) {
+        self.output.push_str("fn contains(&self, obj: $$NAME$$) -> bool { (self.value & obj.value) != 0 }\n");
+        self.output.push_str("fn is_empty(&self) -> bool { self.value == 0 }\n");
+        self.output.push_str("}\n\n");
+    }
+    fn add_operators(&mut self) {
+    }
 }
 
 #[proc_macro_attribute]
 pub fn EnumBitFlags(_args: TokenStream, input: TokenStream) -> TokenStream {
     let mut p = Parser::new();
     p.parse(input);
+    p.add_methods();
     println!("{}",p.output.as_str());
     return TokenStream::new();
 }
